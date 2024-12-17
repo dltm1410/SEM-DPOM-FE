dd45205a03694ff13e9f2025a73f2e95c3dc7683
tuyetmai <duongmai6241@gmail.com>
2024-12-17 17:12:49 +0700

    fix: update staff

--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -125,17 +125,14 @@ const Staff = () => {
                   aria-hidden="true"
                   className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                   fill="currentColor"
-                  viewBox="0 0 20 20"
+                  viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                  />
+                  <path d="M12 2L2 7h3v10h14V7h3L12 2z" />
+                  <path d="M9 21V9h6v12H9z" />
                 </svg>
                 <span className="flex-1 ml-3 text-left whitespace-nowrap">
-                  Manage Products & Variants
+                  Inventory
                 </span>
               </button>
             </ul>
